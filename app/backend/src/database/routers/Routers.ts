e412a2475041f6875722c6afd008e68d178ba99e
Elithon Silva <elithonsilva@gmail.com>
2022-03-17 16:50:42 -0300

    desenvolve class Route para impl rotas do projeto

---
+++ app/backend/src/database/routers/Routers.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+
+export default class Route {
+  public router: express.Router;
+
+  constructor() {
+    this.router = express.Router();
+
+    this.router
+      .route('/login')
+      .get((req, res) => {
+        res.status(200).json({ message: 'Tudo certo!' });
+      });
+  }
+}
