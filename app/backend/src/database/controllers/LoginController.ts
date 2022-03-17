827ae4528928ea0f9a2342d4584054c8df35a464
Elithon Silva <elithonsilva@gmail.com>
2022-03-17 17:50:39 -0300

    inicia implementação do LoginController

--- app/backend/src/database/controllers/LoginController.ts
+++ app/backend/src/database/controllers/LoginController.ts
@@ -1,1 +1,6 @@
-// ...
+import { Request, Response } from 'express';
+
+const Login = (req: Request, res: Response) => res
+  .status(200).json({ message: 'Login efetuado com sucesso.' });
+
+export default Login;
