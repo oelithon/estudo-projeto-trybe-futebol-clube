c073ac8ea8acc0bae8fccb041b1844dcc18db7e7
Elithon Silva <elithonsilva@gmail.com>
2022-03-18 15:50:57 -0300

    add interface UserInfo

--- app/backend/src/database/interfaces/User.ts
+++ app/backend/src/database/interfaces/User.ts
@@ -1,4 +1,10 @@
-export default interface UserLogin {
+export interface LoginInfo {
   email: string;
   password: string;
+}
+
+export interface UserInfo extends LoginInfo {
+  id: number | null;
+  username: string | null;
+  role: string | null;
 }
