3f2a67a0741be93f6b438ed7e594489f0899a0a8
Elithon Silva <elithonsilva@gmail.com>
2022-03-18 16:27:53 -0300

    import fs, jwt, e faz leitura do arquivo jwt.evaluation.key

--- app/backend/src/database/controllers/LoginController.ts
+++ app/backend/src/database/controllers/LoginController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
+import * as fs from 'fs';
+import * as jwt from 'jsonwebtoken';
 import { LoginInfo, UserInfo } from '../interfaces/User';
 import UserModel from '../models/UserModel';
+
+const secret = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
 
 const Login = async (req: Request, res: Response) => {
   const { email }: LoginInfo = req.body;
@@ -8,7 +12,9 @@ const Login = async (req: Request, res: Response) => {
   const userData = await UserModel.findOne({ where: { email } });
   const { id, username, role } = userData as UserInfo;
 
-  res.status(200).json({ user: { id, username, role, email } });
+  const token = jwt.sign({ username }, secret, { algorithm: 'HS256' });
+
+  res.status(200).json({ user: { id, username, role, email }, token });
 };
 
 export default Login;
