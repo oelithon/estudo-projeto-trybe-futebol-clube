5cad3e255691b0a6efa6d12fa2d61d0452ad8ad2
Elithon Silva <elithonsilva@gmail.com>
2022-03-18 15:55:22 -0300

    retorna status 200 com informações de usuário

--- app/backend/src/database/controllers/LoginController.ts
+++ app/backend/src/database/controllers/LoginController.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from 'express';
+import { LoginInfo, UserInfo } from '../interfaces/User';
+import UserModel from '../models/UserModel';
 
-const Login = (req: Request, res: Response) => res
-  .status(200).json({ message: 'Login efetuado com sucesso.' });
+const Login = async (req: Request, res: Response) => {
+  const { email }: LoginInfo = req.body;
+
+  const userData = await UserModel.findOne({ where: { email } });
+  const { id, username, role } = userData as UserInfo;
+
+  res.status(200).json({ user: { id, username, role, email } });
+};
 
 export default Login;
