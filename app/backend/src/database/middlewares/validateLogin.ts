f5edc718e7da9ebebef13b8cb02b6abfd64647e5
Elithon Silva <elithonsilva@gmail.com>
2022-03-19 18:48:06 -0300

    implementa validaçoes de password

--- app/backend/src/database/middlewares/validateLogin.ts
+++ app/backend/src/database/middlewares/validateLogin.ts
@@ -1,28 +1,54 @@
 import { NextFunction, Request, Response } from 'express';
 import * as Joi from 'joi';
+import * as bcrypt from 'bcryptjs';
 import UserModel from '../models/UserModel';
 
-const schema = Joi.object({
+const failEmailOrPass = { message: 'Incorrect email or password' };
+const fieldsFilled = { message: 'All fields must be filled' };
+
+const schemaEmail = Joi.object({
   email: Joi.string().email().required(),
 });
 
-const validateEmail = async (req: Request, res: Response, next: NextFunction) => {
+export const validateEmail = async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
 
-  if (!email) return res.status(401).json({ message: 'All fields must be filled' });
+  if (!email) return res.status(401).json(fieldsFilled);
 
-  const { error } = schema.validate({ email });
+  const { error } = schemaEmail.validate({ email });
 
   if (error) {
-    console.log(error);
-    return res.status(401).json({ message: 'Incorrect email or password' });
+    return res.status(401).json(failEmailOrPass);
   }
 
   const userEmail = await UserModel.findOne({ where: { email } });
 
-  if (!userEmail) return res.status(401).json({ message: 'Incorrect email or password' });
+  if (!userEmail) return res.status(401).json(failEmailOrPass);
 
   next();
 };
 
-export default validateEmail;
+const schemaPass = Joi.object({
+  pass: Joi.string().min(7).required(),
+});
+
+export const validatePassword = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password: pass } = req.body;
+
+  if (!pass) return res.status(401).json(fieldsFilled);
+
+  const { error } = schemaPass.validate({ pass });
+
+  if (error) {
+    return res.status(401).json(failEmailOrPass);
+  }
+
+  const userData = await UserModel.findOne({ where: { email } });
+
+  if (userData) {
+    const matchPass = bcrypt.compareSync(pass, userData.password);
+    if (!matchPass) return res.status(401).json(failEmailOrPass);
+  }
+
+  next();
+};
