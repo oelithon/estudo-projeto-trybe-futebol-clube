c3711a53c6c4879f572ec5fd6e52fc562f5acc58
Elithon Silva <elithonsilva@gmail.com>
2022-03-21 11:38:54 -0300

    testa se o login correto retorna um objeto com informações de usuário e status code 200

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -6,14 +6,18 @@ import { app } from '../app';
 import UserModel from '../database/models/UserModel';
 
 import { Response } from 'superagent';
+import { object } from 'joi';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('As requisições no endpoint .post na rota /login', () => {
+describe('Quando o login foi feito com sucesso.', () => {
+
+  let chaiHttpResponse: Response;
 
   before(async () => {
+
     sinon
       .stub(UserModel, "findOne")
       .resolves({
@@ -23,24 +27,36 @@ describe('As requisições no endpoint .post na rota /login', () => {
 		    email: 'admin@admin.com',
 		    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
       } as UserModel);
+    
+    chaiHttpResponse = await chai.request(app)
+      .post('/login')
+      .send({
+        "email": "admin@admin.com",
+        "password": "secret_admin"
+      });
   });
 
   after(() => {
     (UserModel.findOne as sinon.SinonStub).restore();
   });
 
-  it('deveria retornar status 200', async () => {
-    const result = await chai.request(app).post('/login');
+  it('deveria retornar um objeto com informações do usuário logado', async () => {
+    
+    expect(chaiHttpResponse).to.be.an('object');
+  })
 
-    expect(result).to.have.status(200);
+  it('deveria retornar status 200', async () => {
+
+    expect(chaiHttpResponse).to.have.status(200);
   });
 });
 
 describe('As requisições no endpoint .get na rota /login', () => {
 
-  it('deveria retornar status 200', async () => {
-    const result = await chai.request(app).get('/login');
+  let chaiHttpResponse: Response;
 
-    expect(result).to.have.status(200);
+  it('deveria retornar status 200', async () => {
+    chaiHttpResponse = await chai.request(app).get('/login');
+    expect(chaiHttpResponse).to.have.status(200);
   });
 });
