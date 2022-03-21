a9cfd2d0f9ddc942b8d2bf1b093b800a71772432
Elithon Silva <elithonsilva@gmail.com>
2022-03-21 16:48:04 -0300

    implementa LoginValidate para verificar um token válido ou não

--- app/backend/src/database/controllers/LoginController.ts
+++ app/backend/src/database/controllers/LoginController.ts
@@ -1,20 +1,35 @@
 import { Request, Response } from 'express';
 import * as fs from 'fs';
 import * as jwt from 'jsonwebtoken';
+import Payload from '../interfaces/Token';
 import { LoginInfo, UserInfo } from '../interfaces/User';
 import UserModel from '../models/UserModel';
 
 const secret = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
 
-const Login = async (req: Request, res: Response) => {
+export const Login = async (req: Request, res: Response) => {
   const { email }: LoginInfo = req.body;
 
   const userData = await UserModel.findOne({ where: { email } });
   const { id, username, role } = userData as UserInfo;
 
-  const token = jwt.sign({ username }, secret, { algorithm: 'HS256' });
+  const token = jwt.sign({ username, role }, secret, { algorithm: 'HS256' });
 
   res.status(200).json({ user: { id, username, role, email }, token });
 };
 
-export default Login;
+export const LoginValidate = (req: Request, res: Response) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
+    const payload = jwt.verify(authorization, secret);
+
+    const { role } = payload as Payload;
+
+    return res.status(200).json(role);
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
