e1887fadf30df1ef15a0f0a4a688228d0dcbf8f5
Elithon Silva <elithonsilva@gmail.com>
2022-03-23 14:58:56 -0300

    ajusta chave club_name > clubName

--- app/backend/src/database/models/ClubModel.ts
+++ app/backend/src/database/models/ClubModel.ts
@@ -1,34 +1,22 @@
 import { DataTypes, Model } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
 import Match from './MatchModel';
 
 class Club extends Model {
-  // public <campo>!: <tipo>;
   public id: number;
 
-  public club_name: string;
+  public clubName: string;
 }
 
 Club.init({
-  // ... Campos
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   club_name: { allowNull: false, type: DataTypes.STRING },
 }, {
-  // ... Outras configs
   underscored: true,
   sequelize: db,
-  // modelName: 'example',
   timestamps: false,
   tableName: 'clubs',
 });
-
-/**
-  * `Workaround` para aplicar as associations em TS:
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
 
 Match.belongsToMany(Club, {
   as: 'clubId',
@@ -36,10 +24,5 @@ Match.belongsToMany(Club, {
   foreignKey: 'home_team',
   otherKey: 'away_team',
 });
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
 export default Club;
