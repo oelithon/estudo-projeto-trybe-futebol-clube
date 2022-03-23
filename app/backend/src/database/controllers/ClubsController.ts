ff20233e0e5ad41964f0bfe1a788d96e954c0034
Elithon Silva <elithonsilva@gmail.com>
2022-03-23 12:55:12 -0300

    remove uma quebra de linha desnecessária

--- app/backend/src/database/controllers/ClubsController.ts
+++ app/backend/src/database/controllers/ClubsController.ts
@@ -3,7 +3,6 @@ import ClubModel from '../models/ClubModel';
 
 const getAllClubs = async (_req: Request, res: Response) => {
   const clubsList = await ClubModel.findAll();
-
   return res.status(200).json(clubsList);
 };
 
