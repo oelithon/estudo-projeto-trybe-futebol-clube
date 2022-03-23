5dd45e6c161384d38be94d97ecc8c05fb901dd3a
Elithon Silva <elithonsilva@gmail.com>
2022-03-23 17:27:01 -0300

    impl getClubById

--- app/backend/src/database/controllers/ClubsController.ts
+++ app/backend/src/database/controllers/ClubsController.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express';
 import ClubModel from '../models/ClubModel';
 
-const getAllClubs = async (_req: Request, res: Response) => {
+export const getAllClubs = async (_req: Request, res: Response) => {
   const clubsList = await ClubModel.findAll();
   return res.status(200).json(clubsList);
 };
 
-export default getAllClubs;
+export const getClubByid = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const club = await ClubModel.findByPk(id);
+
+  return res.status(200).json(club);
+};
