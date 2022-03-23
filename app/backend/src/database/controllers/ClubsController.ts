ddfd8c09a2b39002cab7f0105aa8569b6dbf0972
Elithon Silva <elithonsilva@gmail.com>
2022-03-23 10:36:34 -0300

    underscore no req, pois ele não é utilizado

--- app/backend/src/database/controllers/ClubsController.ts
+++ app/backend/src/database/controllers/ClubsController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import ClubModel from '../models/ClubModel';
 
-const getAllClubs = async (req: Request, res: Response) => {
+const getAllClubs = async (_req: Request, res: Response) => {
   const clubsList = await ClubModel.findAll();
 
   return res.status(200).json(clubsList);
