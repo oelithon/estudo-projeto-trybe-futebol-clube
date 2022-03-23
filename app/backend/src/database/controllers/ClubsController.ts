53287731183751caa85c51e46f4a67ed2fce3b45
Elithon Silva <elithonsilva@gmail.com>
2022-03-23 10:35:16 -0300

    impl função getAllClubs que retorna um array de objetos com id e club_name

--- app/backend/src/database/controllers/ClubsController.ts
+++ app/backend/src/database/controllers/ClubsController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
+import ClubModel from '../models/ClubModel';
 
-const getAllClubs = async (req: Request, res: Response) => res
-  .status(200).json({ message: 'all clubs' });
+const getAllClubs = async (req: Request, res: Response) => {
+  const clubsList = await ClubModel.findAll();
+
+  return res.status(200).json(clubsList);
+};
 
 export default getAllClubs;
