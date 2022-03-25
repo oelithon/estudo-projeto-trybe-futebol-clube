cc61cb65cff3c7a826c4c673ef6dfdca7019f548
Elithon Silva <elithonsilva@gmail.com>
2022-03-25 16:33:47 -0300

    add rota /matchs/:id/finish

--- app/backend/src/database/routers/routeMatchs.ts
+++ app/backend/src/database/routers/routeMatchs.ts
@@ -1,12 +1,16 @@
 import * as express from 'express';
 
-import { getAllMatchs, createMatch } from '../controllers/MatchsController';
+import { getAllMatchs, createMatch, finishMatch } from '../controllers/MatchsController';
 
 export default class RouteMatchs {
   public router: express.Router;
 
   constructor() {
     this.router = express.Router();
+
+    this.router
+      .route('/matchs/:id/finish')
+      .patch(finishMatch);
 
     this.router
       .route('/matchs')
