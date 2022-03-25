4f36524769e17cb2d1f4f35c2af7fcb8c7f4282b
Elithon Silva <elithonsilva@gmail.com>
2022-03-25 17:32:17 -0300

    impl validateToken

--- app/backend/src/database/middlewares/validateMatch.ts
+++ app/backend/src/database/middlewares/validateMatch.ts
@@ -1,7 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
+import * as fs from 'fs';
+import * as jwt from 'jsonwebtoken';
+
 import MatchModel from '../models/MatchModel';
 
-const validateIdMatch = async (req: Request, res: Response, next: NextFunction) => {
+const secret = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
+
+export const validateIdMatch = async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
 
   const MatchById = await MatchModel.findByPk(id);
@@ -13,4 +18,15 @@ const validateIdMatch = async (req: Request, res: Response, next: NextFunction) 
   next();
 };
 
-export default validateIdMatch;
+export const validateToken = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) return res.status(401).json({ message: 'Token is required' });
+
+    jwt.verify(authorization, secret);
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+  next();
+};
