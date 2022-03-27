814fd57bc0f2d534d85ef525924a22b7627d88aa
Elithon Silva <elithonsilva@gmail.com>
2022-03-27 16:04:21 -0300

    inicializa express.Router() no constructor

--- app/backend/src/database/routers/routeLeaderboard.ts
+++ app/backend/src/database/routers/routeLeaderboard.ts
@@ -1,11 +1,16 @@
 import * as express from 'express';
+import { Request, Response } from 'express';
 
 export default class RouteLeaderboard {
   public router: express.Router;
 
   constructor() {
+    this.router = express.Router();
+
     this.router
       .route('/leaderboard/home')
-      .get();
+      .get((_req: Request, res: Response) => {
+        res.status(200).json({ message: 'tudo certo' });
+      });
   }
 }
