25c7a83ba49fe48497d6855d1e1cdbcb6d36cb41
Elithon Silva <elithonsilva@gmail.com>
2022-03-27 16:34:36 -0300

    importa e configura Leaderboard no endpoint /leaderboard/home

--- app/backend/src/database/routers/routeLeaderboard.ts
+++ app/backend/src/database/routers/routeLeaderboard.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
+
+import LeaderboardHome from '../controllers/LeaderboardHome';
 
 export default class RouteLeaderboard {
   public router: express.Router;
@@ -9,8 +10,6 @@ export default class RouteLeaderboard {
 
     this.router
       .route('/leaderboard/home')
-      .get((_req: Request, res: Response) => {
-        res.status(200).json({ message: 'tudo certo' });
-      });
+      .get(LeaderboardHome);
   }
 }
