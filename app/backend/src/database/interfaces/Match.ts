7096ddbd20e0dc011578a8c40df8cf52784c0d9e
Elithon Silva <elithonsilva@gmail.com>
2022-03-29 17:15:51 -0300

    impl interface MatchClub extends MatchModel

--- app/backend/src/database/interfaces/Match.ts
+++ app/backend/src/database/interfaces/Match.ts
@@ -1,7 +1,15 @@
+import MatchModel from '../models/MatchModel';
+
 export default interface NewMatch {
   homeTeam: number,
   homeTeamGoals: number,
   awayTeam: number,
   awayTeamGoals: number,
   inProgress: boolean,
+}
+
+export interface MatchClub extends MatchModel {
+  homeClub?: {
+    clubName: string,
+  }
 }
