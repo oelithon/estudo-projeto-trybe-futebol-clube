4a4680402e1335f40eefdd70f99cb5ffd9cf2acb
Elithon Silva <elithonsilva@gmail.com>
2022-03-29 17:46:42 -0300

    impl func listAllMatches que retorna todas as partidas encerradas

--- app/backend/src/database/controllers/LeaderboardHome.ts
+++ app/backend/src/database/controllers/LeaderboardHome.ts
@@ -3,17 +3,22 @@ import { Request, Response } from 'express';
 import MatchModel from '../models/MatchModel';
 import ClubModel from '../models/ClubModel';
 
-const LeaderboardHome = async (_req: Request, res: Response) => {
-  const finishedMatchs = await MatchModel.findAll({
+const listAllMatches = async () => {
+  const list = await MatchModel.findAll({
     where: { inProgress: false },
     attributes: { exclude: ['home_team', 'away_team'] },
     include: [
       { model: ClubModel, as: 'homeClub', attributes: { exclude: ['id'] } },
-      { model: ClubModel, as: 'awayClub', attributes: { exclude: ['id'] } },
     ],
   });
 
-  res.status(200).json(finishedMatchs);
+  return list;
+};
+
+const LeaderboardHome = async (_req: Request, res: Response) => {
+  const listMatches = await listAllMatches();
+
+  res.status(200).json(listMatches);
 };
 
 export default LeaderboardHome;
